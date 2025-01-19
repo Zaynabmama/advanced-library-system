cd9d38a6c0aebb5c5186f179d4f1c17040c18b04
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2025-01-19 18:19:03 +0200

    Add error and success messages

---
+++ src/global/errors/errors.ts
@@ -0,0 +1,9 @@
+export const ErrorMessages = {
+    INVALID_OTP: 'The OTP provided is invalid or expired.',
+    INVALID_PIN: 'The PIN provided is invalid or expired.',
+    USER_NOT_FOUND: 'User not found.',
+    ROLE_NOT_FOUND: 'Role not found.',
+    UNAUTHORIZED: 'You are not authorized to perform this action.',
+    EMAIL_SEND_FAILED: 'Failed to send the email. Please try again later.',
+  };
+  
---
+++ src/global/message.ts
@@ -0,0 +1,9 @@
+export const Messages = {
+    CMS_CREATED: 'CMS user added successfully ,The password has been sent to their email.',
+    ROLE_ASSIGNED:'Role assigned successfully',
+    MEMBER_REGISTRED:'Member registered successfully',
+    OTP_SENDED:'OTP sent to the provided email address.',
+    PIN_GENERATED:'A new PIN code has been generated and sent to the author email.',
+    AUTHOR_CREATED:'Author added successfully ,the PIN code has been sent to their email.'
+};
+  
